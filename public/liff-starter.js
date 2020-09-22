ad6973544e9f46ad85a630768ef082d95834ea8f
Sam Wang <sam.wang@linecorp.com>
2020-09-22 21:05:00 +0800

    handle menu data

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -200,7 +200,8 @@ window.onload = function () {
   M.Modal.getInstance(modals[2]).open()
 
   checkQueries()
-  fetchMenuData()
+  // fetchMenuData()
+  handleMenuData()
   eventListener()
 
   let stepper = document.querySelector('.stepper')
@@ -227,13 +228,105 @@ function fetchMenuData() {
     .then((res) => {
       return res.json()
     })
-    .then((res) => {
-      console.log(res)
-      res = menu_data
-    })
+    .then(handleMenuData)
     .catch((err) => {
       console.log(err)
     })
+}
+
+function handleMenuData(menu) {
+  menu = menu_data
+  date = Object.keys(menu)
+
+  date.map((d, idx) => {
+    console.log(d)
+    const new_step_li = document.createElement('li')
+    new_step_li.className = 'step'
+
+    const new_step_title = document.createElement('div')
+    new_step_title.className = 'step-title waves-effect'
+    new_step_title.innerHTML = new Date(date[0]).toDateString()
+
+    new_step_li.appendChild(new_step_title)
+
+    const new_step_content = document.createElement('div')
+    new_step_content.className = 'step-content'
+
+    const new_card_row = document.createElement('div')
+    new_card_row.className = 'card-row'
+
+    menu[d].map((meal) => {
+      const new_card = document.createElement('div')
+      new_card.className = 'card'
+
+      const new_card_image = document.createElement('div')
+      new_card_image.className = 'card-image'
+
+      const new_card_img = document.createElement('img')
+      new_card_img.src = meal.picurl
+
+      new_card_image.appendChild(new_card_img)
+
+      new_card.appendChild(new_card_image)
+
+      const new_card_content = document.createElement('div')
+      new_card_content.className = 'card-content'
+
+      const new_card_p = document.createElement('p')
+      new_card_p.innerHTML =
+        meal.name + '<br/> <small>by ' + meal.supplier + '</small>'
+
+      new_card_content.appendChild(new_card_p)
+
+      new_card.appendChild(new_card_content)
+
+      new_card_row.appendChild(new_card)
+    })
+
+    new_step_content.appendChild(new_card_row)
+
+    const new_step_actions = document.createElement('div')
+    new_step_actions.className = 'step-actions'
+
+    if (idx !== 0) {
+      const new_btn = document.createElement('button')
+      new_btn.className = 'waves-effect waves-dark btn-flat previous-step'
+      new_btn.innerHTML = '上一天'
+      new_step_actions.appendChild(new_btn)
+    }
+
+    if (idx < date.length - 1) {
+      const new_btn = document.createElement('button')
+      new_btn.className = 'waves-effect waves-dark btn next-step'
+      new_btn.innerHTML = '下一天'
+      new_step_actions.appendChild(new_btn)
+
+      const new_skip_btn = document.createElement('button')
+      new_skip_btn.className =
+        'waves-effect waves-dark btn-flat next-step grey lighten-3'
+      new_skip_btn.innerHTML = '略過'
+      new_step_actions.appendChild(new_skip_btn)
+    } else {
+      const new_btn = document.createElement('a')
+      new_btn.href = '#modal2'
+      new_btn.className = 'waves-effect waves-dark btn next-step'
+      new_btn.innerHTML = '提交'
+      new_step_actions.appendChild(new_btn)
+
+      const new_skip_btn = document.createElement('a')
+      new_skip_btn.href = '#modal2'
+      new_skip_btn.className =
+        'waves-effect waves-dark btn-flat modal-trigger grey lighten-3'
+      new_skip_btn.innerHTML = '略過'
+      new_step_actions.appendChild(new_skip_btn)
+    }
+
+    new_step_content.appendChild(new_step_actions)
+
+    new_step_li.appendChild(new_step_content)
+
+    document.getElementsByClassName('stepper')[0].appendChild(new_step_li)
+  })
 }
 
 function checkQueries() {
