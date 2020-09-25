1b6b86d90f41240c2925646075b05b9f4203cfd1
Sam Wang <sam.wang@linecorp.com>
2020-09-25 11:43:47 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
 const myLiffId = process.env.MY_LIFF_ID
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 app.get('/send-id', function (req, res) {
   if (req.query) res.json({ id: myLiffId })
@@ -14,11 +15,12 @@ app.get('/send-id', function (req, res) {
 
 app.get('/', (req, res) => {
   if (req.query['liff.state']) {
-    res.sendFile(express.static('public/redirect.html'))
+    res.sendFile(path.join(__dirname, './public', '/redirect.html'))
   } else {
-    res.sendFile(express.static('public/index.html'))
+    res.sendFile(path.join(__dirname, './public', '/index.html'))
   }
 })
+
 app.use('/report', express.static('public/report.html'))
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
