34f99ecd95f72791031d70af6c8e98b91c2d7042
Sam Wang <sam.wang@linecorp.com>
2020-09-25 11:46:01 +0800

    update

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 const myLiffId = process.env.MY_LIFF_ID
 
-app.use(express.static('public'))
-
-app.get('/send-id', function (req, res) {
-  if (req.query) res.json({ id: myLiffId })
-})
-
 app.get('/', (req, res) => {
   if (req.query['liff.state']) {
     res.sendFile(path.join(__dirname, './public', '/redirect.html'))
   } else {
     res.sendFile(path.join(__dirname, './public', '/index.html'))
   }
+})
+
+app.use(express.static('public'))
+
+app.get('/send-id', function (req, res) {
+  if (req.query) res.json({ id: myLiffId })
 })
 
 app.use('/report', express.static('public/report.html'))
