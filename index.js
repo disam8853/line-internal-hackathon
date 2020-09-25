c54e502124454155be7c72d9704d43c978f5fd3a
Sam Wang <sam.wang@linecorp.com>
2020-09-25 11:34:39 +0800

    update

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ const myLiffId = process.env.MY_LIFF_ID
 app.use(express.static('public'))
 
 app.get('/send-id', function (req, res) {
-  res.json({ id: myLiffId })
+  if (req.query) res.json({ id: myLiffId })
 })
 
+app.get('/', (req, res) => {
+  if (req.query['liff.state']) {
+    res.sendFile(express.static('public/redirect.html'))
+  }
+  res.sendFile(express.static('public/index.html'))
+})
 app.use('/report', express.static('public/report.html'))
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
